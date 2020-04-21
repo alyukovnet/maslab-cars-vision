b06d1d8db0614e2488e6963030ae30df9df46c03
vova <vla77313222@yandex.ru>
2020-04-21 20:42:00 +0300

    Removing trash

--- detection/dirtDetec/main.cpp
+++ detection/dirtDetec/main.cpp
@@ -5,20 +5,7 @@
 #include <opencv2/imgproc.hpp>
 using namespace std;
 using namespace cv;
-void help()
-{
-    cout
-        << "----------------------------------------------------" << endl
-        << "This is a test program for the Image Watch Debugger " << endl
-        << "plug-in for Visual Studio. The program loads an     " << endl
-        << "image from a file and runs the Canny edge detector. " << endl
-        << "No output is displayed or written to disk."
-        << endl
-        << "Usage:" << endl
-        << "image-watch-demo inputimage" << endl
-        << "----------------------------------------------------" << endl
-        << endl;
-}
+
 int main(int argc, char *argv[])
 {
     Mat framel, dframe,th;
