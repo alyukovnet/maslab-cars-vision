897e5855866c748b36aa2d35f60bf27efabd25e3
Danila Alyukov <im@alyukovnet.ru>
2020-04-28 23:19:55 +0500

    ColorCast detection

--- colorCast/correction.cpp
+++ colorCast/correction.cpp
@@ -1,23 +1,62 @@
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+
 #include "correction.h"
+
 using namespace cv;
 
 
-void colorCastCorrection(Mat& input, Mat& output) {
-    float k[3];
-    k[0] = 1; // Blue
-    k[1] = 0; // Green
-    k[2] = 1; // Red
-    applyColorsCoef(input, output, k);
+ColorCast::ColorCast() {
+    CastFactor_ = 0;
+}
+
+int ColorCast::detect(Mat& input) {
+    // averageChrominanceAndMomentum
+    cvtColor(input, inputLab_, COLOR_BGR2Lab);
+    Scalar Mat_mean = mean(input);
+    mean_a_ = Mat_mean[1] - 128;
+    mean_b_ = Mat_mean[2] - 128;
+
+    Mat diffMat(input.rows, input.cols, CV_8UC3, Mat_mean);
+    absdiff(input, Mat_mean, diffMat); // diff Input image and mean
+    Scalar diffMat_mean = mean(diffMat);
+    M_a_ = diffMat_mean[1];
+    M_b_ = diffMat_mean[2];
+
+    D_ = sqrt(mean_a_*mean_a_ + mean_b_*mean_b_);
+    M_ = sqrt(M_a_*M_a_ + M_b_*M_b_);
+    CastFactor_ = D_ / M_;
+
+    // castDecision
+    if (1.0 <= CastFactor_ && CastFactor_ <= 2.0) {
+        return NO_CAST;
+    } else if ((mean_a_ >= -4.0) && (mean_a_ <= 4.0) && (mean_b_ >= -4.0) && (mean_b_ <= 4.0)) {
+        return MILD;
+    } else if (CastFactor_ < 1.0) {
+        return INCOMPATIBLE_CAMERA;
+    } else if (CastFactor_ > 2.0) {
+        return DETECTED;
+    }
+}
+
+double ColorCast::getCastFactor() {
+    return CastFactor_;
+};
+
+void ColorCast::correct(Mat& output) {
+
+//    Mat diffMat(input.rows, input.cols, CV_8UC3, Scalar(0, ));
+
+  //  cvtColor(inputLab_, output, COLOR_Lab2BGR);
+}
+
+ColorCast::~ColorCast() {
+
+    return;
 }
 
 
-void applyColorsCoef(Mat& input, Mat& output, float k[3]) {
-    for (int i = 0; i < input.rows; i++) {
-        for (int j = 0; j < input.cols; j++) {
-            output.at<Vec3b>(i, j)[0] = input.at<Vec3b>(i, j)[0] * k[0];
-            output.at<Vec3b>(i, j)[1] = input.at<Vec3b>(i, j)[1] * k[1];
-            output.at<Vec3b>(i, j)[2] = input.at<Vec3b>(i, j)[2] * k[2];
-        }
-    }
+void castVariator(Mat& src, Mat& dest, int Red, int Green, int Blue) {
+    Mat mask(src.rows, src.cols, CV_8UC3, Scalar(Blue, Green, Red));
+    multiply(src, mask, dest, 0.01);
 }
--- colorCast/correction.h
+++ colorCast/correction.h
@@ -14,22 +14,54 @@
 using namespace cv;
 
 
-/*!
-Устраняет проблему ColorCast
-\param[in] input Входной кадр (объект cv::Mat) BGR
-\param[out] output Результат обработки (объект cv::Mat)
-*/
-void colorCastCorrection(Mat& input, Mat& output);
+class ColorCast {
+private:
+    double CastFactor_;  //!< Описание
+    double mean_a_;      //!< Mean of channel a
+    double mean_b_;      //!< Mean of channel b
+    double D_;           //!< Описание
+    double M_a_;         //!< Описание
+    double M_b_;         //!< Описание
+    double M_;           //!< Описание
+    Mat inputLab_;       //!< Lab image
+public:
+    /*!
+    Конструктор класса ColorCast
+    \param[in] input Входной кадр (объект cv::Mat) BGR
+    \param[out] output Выходной кадр (объект cv::Mat) BGR
+    */
+    ColorCast();
+
+    //! Результаты обнаружения ColorCast
+    enum ColorCastFlags {
+        NO_CAST = 0,              //!< No cast
+        MILD = 1,                 //!< Color Cast is normal
+        DETECTED = 2,             //!< You can use ColorCast correction for frame
+        INCOMPATIBLE_CAMERA = 3,  //!< You can't use ColorCast correction
+    };
+
+    /*!
+    Обнаружение ColorCast
+    \param[out] output Код ошибки
+    */
+    int detect(Mat& input);
+
+    /*!
+    Обнаружение ColorCast
+    \param[out] output Код ошибки
+    */
+    double getCastFactor();
+
+    /*!
+    Устраняет проблему ColorCast
+    */
+    void correct(Mat& output);
+
+    ~ColorCast();
+};
 
 
-/*!
-Умножает коэффициенты (3) соответсвенно на значение
- каждого канала пикселя
-\param[in] input Входной кадр (объект cv::Mat) BGR
-\param[out] output Результат обработки (объект cv::Mat) BGR
-\param[in] k Коэффициенты для 3-х каналов
-*/
-void applyColorsCoef(Mat& input, Mat& output, float k[3]);
+void castVariator(Mat& dest, Mat& src, int Red, int Green, int Blue);
 
 
 #endif //MASLAB_CARS_VISION_COLORCAST_CORRECTION_H
